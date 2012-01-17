2fb0a0cbbb1fa9c2fa1418bf79cc04cf4f5b3045
harish <harishbayyavarapu@gmail.com>
2012-01-17 02:22:47 -0500

    Added a new change to the println

--- src/come/vanila/sample/HelloWorld.java
+++ src/come/vanila/sample/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		System.out.println("Successfully pushed to Git Repository");
 			
 	}
 
